d242bfd09f8d80a0cf1059ebbc65ffed6ef4cff3
Gul Rasulzada <guldastayr@code.edu.az>
2024-10-30 10:34:58 +0400

    refactor: "Drink Water updated"

--- Drink Water/script.js
+++ Drink Water/script.js
@@ -1,24 +1,49 @@
-const smallCups = document.querySelectorAll('.cup-small')
-const liters = document.getElementById('liters')
-const percentage = document.getElementById('percentage')
-const remained = document.getElementById('remained')
+const smallCups = document.querySelectorAll(".cup-small");
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
 
 smallCups.forEach((cup, index) => {
-  cup.addEventListener('click', () => highlightCups(index))
-})
+  cup.addEventListener("click", () => highlightCups(index));
+});
 const highlightCups = (index) => {
   if (
-    smallCups[index].classList.contains('full') &&
-    !smallCups[index].nextElementSibling.classList.contains('full')
+    smallCups[index].classList.contains("full") &&
+    !smallCups[index].nextElementSibling.classList.contains("full")
   ) {
-    idx--
+    index--;
   }
 
-  smallCups.forEach((cup, idx) => {
-    if (idx <= index) {
-      cup.classList.add('full')
+  smallCups.forEach((cup, indx) => {
+    if (indx <= index) {
+      cup.classList.add("full");
     } else {
-      cup.classList.remove('full')
+      cup.classList.remove("full");
     }
-  })
-}
+  });
+  updateBigCup();
+};
+const updateBigCup = () => {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+  if (totalCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    if ((fullCups / totalCups) * 100 == 0) {
+      percentage.innerText = "";
+    } else {
+      percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+    }
+    // percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+  if (fullCups === totalCups) {
+    percentage.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
+};
